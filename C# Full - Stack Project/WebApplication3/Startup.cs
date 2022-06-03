5b59c49cbce6fd894e3c1dd64e6fbd60871411d5
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-06-03 12:57:40 +0300

    Add files via upload

---
+++ C# Full - Stack Project/WebApplication3/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public DateTime startup = DateTime.Now;
+        public string ComputerName { get; set; }
+        public string Month { get; set; }
+        public string Year { get; set; }
+        public float TotalHours { get; set; }
+    }
+}
