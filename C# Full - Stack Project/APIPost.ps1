1d8cbaa28c22caad49d18da9d2c29889b3c73570
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-07-10 12:28:51 +0300

    Delete C# Full - Stack Project directory

--- C# Full - Stack Project/APIPost.ps1
+++
@@ -1,6 +0,0 @@
-﻿## --- Created by Matan Barhoum --- ##
-$Date = Get-Date
-$Body = @{ComputerName="UserNameTest123";Month=$Date.Month.ToString();Year=$Date.Year.ToString();TotalHours=12345}
-Invoke-WebRequest -Uri https://url:port/api/EmpStartup -Method POST -Body $Body
-Invoke-WebRequest -Uri https://url:port/api/Emp -Method POST -Body $Body
-## --- Created by Matan Barhoum --- ##
